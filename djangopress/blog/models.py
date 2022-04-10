866ec6de56725994e7cdb7ba0a84df6dac8b5a26
Argentinier <argentinier@protonmail.com>
2022-04-10 16:12:50 -0300

    A few changes to Post model and customizing the PostAdmin model

--- djangopress/blog/models.py
+++ djangopress/blog/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 
 
 class TimestampsMixin(models.Model):
-    create_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
@@ -22,7 +22,7 @@ class Post(TimestampsMixin):
     # First value is the Key stored in the Model, second is the human-readable value.
     # If 'None' is set it replaces the '-------'. But in this case I'm using a default, so doesn't matter anyway.
     STATUS_CHOICES = (
-        ('None', 'Select the Post Status'),
+        (None, 'Select the Post Status'),
         (DRAFT, 'Draft'),
         (PUBLISHED, 'Published')
     )
@@ -30,7 +30,7 @@ class Post(TimestampsMixin):
     title = models.CharField(max_length=250)
 
     # unique_for_date means only one slug with the same name will be allowed per day
-    slug_name = models.SlugField(max_length=250, unique_for_date='publish')
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     body = models.TextField(max_length=5000)
     publish = models.DateTimeField(default=timezone.now)
