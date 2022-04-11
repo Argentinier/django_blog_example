3efa4717b2b5ae05e54a32246d672bb78587da86
Argentinier <argentinier@protonmail.com>
2022-04-10 23:13:29 -0300

    Implement List and Detail Views for Post model, urlpatterns to reach those Views and HTML templates to render the data

--- djangopress/blog/models.py
+++ djangopress/blog/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 
 
@@ -56,3 +57,12 @@ class Post(TimestampsMixin):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse(
+            viewname='blog:post_detail',
+            args=[self.publish.year,
+                  self.publish.month,
+                  self.publish.day,
+                  self.slug]
+        )
--- djangopress/blog/views.py
+++ djangopress/blog/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
+from .models import Post
+
+
+def post_list(request):
+    posts = Post.published.all()
+
+    return render(
+        request=request,
+        template_name='blog/post/list.html',
+        context={'posts': posts}
+    )
+
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(
+        Post,
+        slug=post,
+        status=Post.PUBLISHED,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day
+    )
+
+    return render(
+        request=request,
+        template_name='blog/post/detail.html',
+        context={'post': post}
+    )
