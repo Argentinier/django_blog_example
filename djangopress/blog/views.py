fdf26f9a686b13a50e521838e6a8d28e64a386a0
Argentinier <argentinier@protonmail.com>
2022-04-13 09:42:38 -0300

    Ability to share a post via email

--- djangopress/blog/views.py
+++ djangopress/blog/views.py
@@ -1,9 +1,11 @@
+from django.core.mail import send_mail
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from django.views.generic import ListView
 
+from forms import EmailPostForm
 from .models import Post
 
 
@@ -12,6 +14,35 @@ class PostListView(ListView):
     context_object_name = 'posts'
     paginate_by = 3
     template_name = 'blog/post/list.html'
+
+
+def post_share(request, post_id):
+    # Retrieve Post by ID
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+
+        if form.is_valid():
+            cleaned_data = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"Read this: {post.title}"
+            sent = bool(send_mail(
+                subject=subject,
+                message=f"{cleaned_data['email']} sent you this post {post_url}",
+                from_email='system@my_site.com',
+                recipient_list=[cleaned_data['to']],
+                fail_silently=False))
+
+    else:
+        form = EmailPostForm()
+
+    return render(
+        request=request,
+        template_name='blog/post/share.html',
+        context={'post': post, 'form': form, 'sent': sent}
+    )
 
 
 def post_list(request):
